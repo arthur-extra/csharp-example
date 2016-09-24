9e67b8ca549fa79ac040b240f16b120e08f7890b
arthur neuman <arthur.neuman@gmail.com>
2016-09-24 17:05:59 -0400

    initial commit

---
+++ CSharpCalculator/CSharpCalculator/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpCalculator
+{
+    interface ICalculator
+    {
+        int add(int operand1, int operand2);
+        int subtract(int operand1, int operand2);
+        int multiply(int operand1, int operand2);
+        int divide(int operand1, int operand2);
+    }
+}
