07c38e6ab90bcdb3780908b9753b21944d3cb740
arthur neuman <arthur.neuman@gmail.com>
2016-09-27 17:31:17 -0400

    adding calculator implementation

---
+++ CSharpCalculator/CSharpCalculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpCalculator
+{
+    class Calculator : ICalculator
+    {
+        public int add(int operand1, int operand2)
+        {
+            return operand1 + operand2;
+        }
+
+        public int divide(int operand1, int operand2)
+        {
+            return operand1 / operand2;
+        }
+
+        public int multiply(int operand1, int operand2)
+        {
+            return operand1 * operand2;
+        }
+
+        public int subtract(int operand1, int operand2)
+        {
+            return operand1 - operand2;
+        }
+    }
+}
