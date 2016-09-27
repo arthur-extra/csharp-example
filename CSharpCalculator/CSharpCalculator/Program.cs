0a8f732553daab7c1a05b8d910033a854ee81d77
arthur neuman <arthur.neuman@gmail.com>
2016-09-27 17:40:29 -0400

    demonstrate calculator and UI

--- CSharpCalculator/CSharpCalculator/Program.cs
+++ CSharpCalculator/CSharpCalculator/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            ICalculator calculator = new Calculator();
+            UserInterface ui = new UserInterface(calculator);
+            ui.demo();
         }
     }
 }
